c70b476fca948c422465084e6a8edf50933035e4
David Kleckner <David@engr2-30-183-dhcp.int.colorado.edu>
2015-04-16 12:57:03 -0600

    test.cpp added

---
+++ Test.cpp
@@ -0,0 +1,13 @@
+//test
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double a, b;
+int main()
+{
+	a = (1.0/3);
+	b = (1.0/4);
+	cout<<setprecision(1)<<setprecision(2)<<fixed;
+	cout<<"a: "<< a << " b: "<< b<<endl;
+}
